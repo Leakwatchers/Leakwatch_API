29e45549f9c75eba52f216137388c36665cac854
pedroHPlins <pedrohenriquenascimentolins@gmail.com>
2025-10-21 13:26:56 -0300

    Endpoint para checar funcionalidade da API ('/api')

---
+++ src/main/java/com/unifor/br/leakwatch/controller/helloController.java
@@ -0,0 +1,12 @@
+package com.unifor.br.leakwatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+    @GetMapping("/api")
+    public String hello() {
+        return "API tá tudo ok Paizão! Pode usar";
+    }
+}
