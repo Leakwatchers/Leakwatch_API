1a6d02f6a814f0035a487bce6fe3ebc5c87b1caa
pedroHPlins <119020609+pedroHPlins@users.noreply.github.com>
2025-10-31 19:16:25 -0300

    Delete Downloads/Leakwatch_API-master/Leakwatch_API-master directory

--- Downloads/Leakwatch_API-master/Leakwatch_API-master/src/main/java/com/unifor/br/leakwatch/repository/reportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unifor.br.leakwatch.repository;
-
-import com.unifor.br.leakwatch.model.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-    List<Report> findBySensorId(Long sensorId);
-}
--- Downloads/Leakwatch_API-master/Leakwatch_API-master/src/main/java/com/unifor/br/leakwatch/repository/sensorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unifor.br.leakwatch.repository;
-
-import com.unifor.br.leakwatch.model.sensor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface sensorRepository extends JpaRepository<sensor, Long> {
-
-}
